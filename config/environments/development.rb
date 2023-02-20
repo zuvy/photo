d32c141a4d69dc3af255d49c75663f9d463e591f
Joe Zuvanich <zuvanjo@tulsaschools.org>
2023-02-19 18:15:44 -0600

    fixed action mailer in development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
   config.action_mailer.delivery_method = :test
-  config.action_mailer.default_url_options = [ :host => 'http://localhost:3005' ]
+  config.action_mailer.default_url_options = { :host => 'http://localhost:3005' }
   # Show full error reports.
   config.consider_all_requests_local = true
 
